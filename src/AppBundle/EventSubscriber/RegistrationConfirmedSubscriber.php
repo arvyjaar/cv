b34c2b198c856d92b937db71377667d5f44d65d3
Arvydas J <arvydo.j@gmail.com>
2017-04-14 19:01:47 +0000

    Not working FOSUser registration confirmed event subscriber

---
+++ src/AppBundle/EventSubscriber/RegistrationConfirmedSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arvydas
+ * Date: 4/14/17
+ * Time: 8:39 PM
+ */
+
+namespace AppBundle\EventSubscriber;
+
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+
+class RegistrationConfirmedSubscriber implements EventSubscriberInterface
+{
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::REGISTRATION_CONFIRM => 'onRegistrationConfirm'
+        );
+    }
+
+    public function onRegistrationConfirm(GetResponseUserEvent $event)
+    {
+        // TODO: here generate different url's for different ROLE
+
+        $url = $this->router->generate('index_ads');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
