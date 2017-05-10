2acf23dc591982f2b2436e5899e58898429bed52
Arvydas J <arvydo.j@gmail.com>
2017-05-10 16:58:19 +0000

    Removed links

---
+++ src/AppBundle/DataFixtures/Seed/LoadSectorData.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Sector;
+
+class LoadSectorData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function getOrder()
+    {
+        // the order in which fixtures will be loaded
+        return 1;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $sectors = [
+            'Kompiuteriai/IT/internetas',
+            'Mobilusis, fiksuotas ryšys',
+            'Medicina/sveikatos apsauga/farmacija',
+            'Administravimas/sekretoriavimas',
+            'Apsauga',
+            'Apskaita/finansai/auditas',
+            'Dizainas/architektūra',
+            'Draudimas',
+            'Eksportas',
+            'Energetika/elektronika',
+            'Inžinerija/mechanika',
+            'Klientų aptarnavimas/paslaugos',
+            'Maisto gamyba',
+            'Marketingas/reklama',
+            'Nekilnojamasis turtas',
+            'Pardavimų vadyba',
+            'Personalo valdymas',
+            'Pirkimai/tiekimas',
+            'Pramonė/gamyba',
+            'Žemės ūkis/žuvininkystė',
+            'Valstybės tarnyba',
+            'Žiniasklaida/viešieji ryšiai',
+            'Transporto/logistikos vadyba',
+            'Švietimas/mokymai/kultūra',
+            'Statyba',
+            'Prekyba - konsultavimas',
+            'Teisė',
+            'Transporto vairavimas'
+        ];
+        foreach ($sectors as $sector) {
+            $entry = new Sector();
+            $entry->setTitle($sector);
+            $manager->persist($entry);
+        }
+        $manager->flush();
+    }
+}
